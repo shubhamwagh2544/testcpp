fb4e47812183b0bd587344e33cea75e02d968c73
Shubh <shubhamwagh2544@gmail.com>
2026-04-19 11:08:24 +0530

    init

--- TestCPP/main.cpp
+++ TestCPP/main.cpp
@@ -1,14 +1,6 @@
-//
-//  main.cpp
-//  TestCPP
-//
-//  Created by Shubham Wagh on 4/19/26.
-//
+#include "iostream"
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+int main() {
+    std::cout<< "Hello World!" << std::endl;
+    return 0;
 }
