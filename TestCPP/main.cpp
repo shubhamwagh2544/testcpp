11d551893a622d6f8f04783a114cf8935f6b5cb9
Shubh <shubhamwagh2544@gmail.com>
2026-04-19 10:52:58 +0530

    Initial Commit

---
+++ TestCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestCPP
+//
+//  Created by Shubham Wagh on 4/19/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
